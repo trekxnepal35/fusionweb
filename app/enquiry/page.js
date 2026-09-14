61695ec310158223d1a24aa7a9b11caff487bee3
trekxnepal35 <trekxnepal35@gmail.com>
2026-09-14 14:04:52 +0545

    Removed pre modified of booking and endquire, become original form

--- app/enquiry/page.js
+++ app/enquiry/page.js
@@ -1,5 +1,5 @@
 "use client";
-export const dynamic = "force-dynamic";
+
 import { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
